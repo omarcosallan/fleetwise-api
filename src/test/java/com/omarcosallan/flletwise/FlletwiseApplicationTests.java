278ebd8e24b6d2a37b88caf3a516f3bb05d91165
Marcos Allan <marcosallanm1to@gmail.com>
2024-12-26 09:41:07 -0300

    feat: projeto criado

---
+++ src/test/java/com/omarcosallan/flletwise/FlletwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omarcosallan.flletwise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlletwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
