321c30971de961a38c9baba887c5cde03a058c96
Marcos Allan <marcosallanm1to@gmail.com>
2025-01-03 11:56:04 -0300

    feat: docker file deploy

--- src/test/java/com/omarcosallan/fleetwise/FleetwiseApplicationTests.java
+++ src/test/java/com/omarcosallan/fleetwise/FleetwiseApplicationTests.java
@@ -2,8 +2,10 @@ package com.omarcosallan.fleetwise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("teste")
 class FleetwiseApplicationTests {
 
 	@Test
